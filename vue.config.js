9eea9aadfbc21479a17dd182529ceec8375ba9e4
oswin-jerome <oswinjeromej@gmail.com>
2021-09-20 23:11:46 +0530

    Root change

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/resume-generator':'/'
+  // publicPath: process.env.NODE_ENV === 'production'
+  // ? '/resume-generator':'/'
+  publicPath:'/resume-generator'
 }
