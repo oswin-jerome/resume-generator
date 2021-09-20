02434c1a3dac716adc6cd33219c29bbcecfe7615
oswin-jerome <oswinjeromej@gmail.com>
2021-09-20 17:34:18 +0000

    Deploying to gh-pages from @ oswin-jerome/resume-generator@3c342e37819ff26befb837a87c3df29a991f5dbb 🚀

---
+++ precache-manifest.96b5218793f43fdc7a4f3517ac648992.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c82795e64c56f6fd6bdb",
+    "url": "/resume-generator/css/app.e3e30fed.css"
+  },
+  {
+    "revision": "14f1c1290130b0d20917829d89aed257",
+    "url": "/resume-generator/index.html"
+  },
+  {
+    "revision": "c82795e64c56f6fd6bdb",
+    "url": "/resume-generator/js/app.9b57dbef.js"
+  },
+  {
+    "revision": "70fbddf9648f14a64dd6",
+    "url": "/resume-generator/js/chunk-0c78ed8c.b1fe5550.js"
+  },
+  {
+    "revision": "1c54ccca6a0d28eff83c",
+    "url": "/resume-generator/js/chunk-2d22d746.eb8d971b.js"
+  },
+  {
+    "revision": "a99b429fe06b1990b829",
+    "url": "/resume-generator/js/chunk-vendors.567a36f3.js"
+  },
+  {
+    "revision": "205214b630931258b409679d5e239827",
+    "url": "/resume-generator/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/resume-generator/robots.txt"
+  }
+]);
